5e6affe6b7ddf6760511ab4ce0f4e1131f8b260b
kamlakant <kamlakant.shukla@yahoo.com>
2019-10-17 15:05:56 +0530

    Added Eureka server and ribbon balanced microservices

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# spring-cloud-ribbon-demo
-Demo of client side load balancing by Ribbon, using Eureka for service discovery and Config for externalized configuration
+# Spring Cloud Ribbon Demo
+Demo of client side load balancing by Ribbon, using Eureka for service discovery.
+
+## Running the Eureka Server
+
+1. Navigate to `eureka-server` directory.
+2. Execute `mvn spring-boot:run` in terminal to start Eureka server.
+
+## Running the Microservices
+
+1. Navigate to `alice-service` directory.
+2. Open two terminals and execute below commands to start multiple instances of ALICE service.
+```
+mvn spring-boot:run -Dserver.port=9001
+mvn spring-boot:run -Dserver.port=9002
+```
+3. Navigate to `bob-service` directory.
+4. Open two terminals and execute below commands to start multiple instances of BOB service.
+```
+mvn spring-boot:run -Dserver.port=10001
+mvn spring-boot:run -Dserver.port=10002
+```
+5. Open Eureka console at `http://localhost:8001/` and see that 2 instances of both services have been registered.
+![Eureka Server](/screenshots/eureka-server.png)
+6. Open browser and go to `http://localhost:9001/get-stuff/BOB` to fetch data from BOB service into ALICE service. Notice that in the response `Stuff by Bob <port>`, the port number keeps changing in round-robin fashion due to load balancing by Ribbon.
+7. Open browser and go to `http://localhost:10001/get-stuff/ALICE` to fetch data from ALICE service into BOB service. Notice that in the response `Stuff by Alice <port>`, the port number keeps changing in round-robin fashion due to load balancing by Ribbon.
---
+++ alice-service/src/main/java/com/cloud/example/ServiceAlice.java
@@ -0,0 +1,50 @@
+package com.cloud.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class ServiceAlice {
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAlice.class, args);
+    }
+
+    @GetMapping("/generate-stuff")
+    public String generateStuff() {
+        return "Stuff by Alice " + serverPort;
+    }
+
+    @GetMapping("/get-stuff/{serviceName}")
+    public String generateStuff(@PathVariable("serviceName") String serviceName) {
+        return getServiceResponse(serviceName, String.class);
+    }
+
+    private <T> T getServiceResponse(String serviceName, Class<T> responseType) {
+        return restTemplate.getForObject("http://{serviceName}/generate-stuff", responseType, serviceName);
+    }
+
+}
